09061ef5c84c308c35252831c4eca6cc4359b244
大帅哥 <ritenxun@qq.com>
2018-11-10 13:05:32 +0800

    User实体类上传

--- git_day01/src/main/java/pojo/User.java
+++ git_day01/src/main/java/pojo/User.java
@@ -15,6 +15,14 @@ public class User {
                 '}';
     }
 
+    public String getCat() {
+        return cat;
+    }
+
+    public void setCat(String cat) {
+        this.cat = cat;
+    }
+
     public String getName() {
         return name;
     }
